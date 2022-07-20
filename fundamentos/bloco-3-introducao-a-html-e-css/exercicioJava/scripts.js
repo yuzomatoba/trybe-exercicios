6f75e254fa5ed53d014be5bf69ce989bc2a2cd1c
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2022-07-20 18:29:09 -0300

    Mensagem descrevendo alterações

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/exercicioJava/scripts.js
@@ -0,0 +1,21 @@
+const myName = "Yuzo";
+const birthCity = "São Bernardo do Campo";
+let birthYear = 1983;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId, typeof isEnrolled, typeof patientInfo, typeof patientEmail);
+console.log(typeof patientAge);
+const base = 5;
+let height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2*base+2*height;
+console.log(perimeter);
