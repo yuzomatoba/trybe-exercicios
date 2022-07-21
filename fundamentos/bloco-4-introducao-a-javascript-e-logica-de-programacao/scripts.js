37e5b3fab27c26d345cdb9871f30807330e0e599
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2022-07-21 15:26:22 -0300

    exercicios 4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -1,30 +1,12 @@
-let num1 = 5;
-let num2 = 3;
-let result;
-let operator = '-';
+let num1 = 11;
+let num2 = 9;
+let num3 = 8;
 
-switch (operator) {
-   case '+':
-   result = num1 + num2;
-   console.log(result);
-   break;
-
-case '-':
-    result = num1 - num2;
-    console.log(result);
-    break;
-
-case '*':
-    result = num1 * num2;
-    console.log(result);
-
-case '/':
-    result = num1 / num2;
-    console.log(result);
-
-case '%':
-    result = num1 % num2;
-    console.log(result);
-
-default:
-    console.log("Operação não encontrada");}
+if(num1 > num2 && num1 > num3) {
+   console.log(num1);
+} else if( num2 > num1 && num2 > num3) {
+    console.log(num2);
+    
+} else if (num3 > num1 && num3 > num2) {
+    console.log(num3);
+}
